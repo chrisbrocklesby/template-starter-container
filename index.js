ed0546c7727083899491714dcb9f1631518aa627
Chris Brocklesby <chrisbrocklesby@users.noreply.github.com>
2022-09-28 14:20:45 +1300

    deploy

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ http.createServer((request, response) => {
     200,
     { "Content-Type": "text/plain" },
   );
-  response.end("Welcome to your containerized Node.js app!\n");
+  response.end("Welcome to your containerized Node.js app!\n v1");
 }).listen(process.env.PORT || 3003);
